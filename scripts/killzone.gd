2c64cd9b5950044b5b01b781712e603b7578a75a
deepakdnayak <deepakdnayak2004@gmail.com>
2024-08-26 14:25:45 +0530

    score done

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()  # begins the timer
 
 func _on_timer_timeout() -> void:  # on timer hit this runs
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()  # reloads the main scene - start again
